88bb09085feab087398221c33c823722c1b63801
alexgan <alex.gan@mail.utoronto.ca>
2022-04-18 15:23:17 -0400

    added SVM.py

--- SVM.py
+++ SVM.py
@@ -1,15 +1,11 @@
-# Machine learning
 from sklearn.svm import SVC
 from sklearn import svm
-from sklearn.metrics import accuracy_score
   
-# For data manipulation
 import pandas as pd
 import numpy as np
 
-# To plot
-import matplotlib.pyplot as plt
-plt.style.use('seaborn-darkgrid')
+# import matplotlib.pyplot as plt
+# plt.style.use('seaborn-darkgrid')
 
 
 
@@ -25,22 +21,19 @@ def simple_SVM():
     # target vars (feature vectors Y)
     y = np.where(df['Close'].shift(-1) > df['Close'], 1, -1)
     
-    split = int(0.8 * len(df))
+    split = int(0.8 * len(df)) 
     
     X_train = X[:split]
     y_train = y[:split]
     X_test = X[split:]
     y_test = y[split:]
 
-    # # Support vector classifier
-    # cls = SVC().fit(X_train, y_train)
-
-    # df['sig'] = cls.predict(X)
-    # print(cls.score(X_test, y_test))
+    # fit our SVM model and calculate its score 
     rbf_svm = svm.SVC(kernel='rbf')
     rbf_svm.fit(X_train, y_train)
-    score = rbf_svm.score(X_test, y_test)
-    print(score)
+    # mean accuracy on the given test data and labels
+    score = rbf_svm.score(X_test, y_test) 
+    print("mean acc =",score)
 
 
 def main():
